a622f12552f4140a97224eb4acff1a034b587cb6
sjraad <selim@raadtown.com>
2016-09-19 12:30:53 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -21,7 +21,7 @@ plot(DateTime, Voltage, type = "l", xlab = "", ylab = "Voltage")
 plot(DateTime, subMetering1, type = "l", xlab = "", ylab = "Energy Sub metering")
 lines(DateTime, subMetering2, type = "l", col = "red")
 lines(DateTime, subMetering3, type = "l", col = "blue")
-legend("topright", lty= 1, col = c("Black", "red", "blue"), legend = c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("Black", "red", "blue"), lty= 1, cex = 0.4)
 plot(DateTime, ReactivePower, type = "l", xlab = "", ylab = "Global_reactive_power")
 
 # Write to PNG file
