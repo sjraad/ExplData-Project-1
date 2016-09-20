de61bed063fbb66eeb6373507860cd42eb9c3a2e
sjraad <selim@raadtown.com>
2016-09-20 13:56:35 -0400

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -1,6 +1,5 @@
 # read file
-data <- read.table("./Exploratory Data Analysis/Project 1/household_power_consumption.txt", sep = ";", na.strings = "?")
-names(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+data <- read.table("./Exploratory Data Analysis/Project 1/household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
 selectedData <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
 
 # Convert date/time to classes
