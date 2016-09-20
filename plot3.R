2dbd02409027acc7fe3b7c462547d11ee7ea09de
sjraad <selim@raadtown.com>
2016-09-20 13:57:35 -0400

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -1,6 +1,5 @@
 # read file
-data <- read.table("./Exploratory Data Analysis/Project 1/household_power_consumption.txt", sep = ";", na.strings = "?", stringsAsFactors = FALSE)
-names(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+data <- read.table("./Exploratory Data Analysis/Project 1/household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
 selectedData <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
 
 # Convert date/time to classes
@@ -16,7 +15,7 @@ subMetering3 <- as.numeric(selectedData$Sub_metering_3)
 plot(DateTime, subMetering1, type = "l", xlab = "", ylab = "Energy Sub metering")
 lines(DateTime, subMetering2, type = "l", col = "red")
 lines(DateTime, subMetering3, type = "l", col = "blue")
-legend("topright", c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("Black", "red", "blue"), lty= 1, cex = 0.7)
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, col = c("Black", "red", "blue"))
 
 # Write to PNG file
 dev.copy(png, file = "./Exploratory Data Analysis/Project 1/plot3.png", width = 480, height = 480)
--- plot4.R
+++ plot4.R
@@ -1,6 +1,5 @@
 # read file
-data <- read.table("./Exploratory Data Analysis/Project 1/household_power_consumption.txt", sep = ";", na.strings = "?", stringsAsFactors = FALSE)
-names(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+data <- read.table("./Exploratory Data Analysis/Project 1/household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
 selectedData <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
 
 # Convert date/time to classes
@@ -21,9 +20,9 @@ plot(DateTime, Voltage, type = "l", xlab = "", ylab = "Voltage")
 plot(DateTime, subMetering1, type = "l", xlab = "", ylab = "Energy Sub metering")
 lines(DateTime, subMetering2, type = "l", col = "red")
 lines(DateTime, subMetering3, type = "l", col = "blue")
-legend("topright", c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("Black", "red", "blue"), lty= 1, cex = 0.4)
+legend("topright", c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("Black", "red", "blue"), lty= 1, cex = 0.2)
 plot(DateTime, ReactivePower, type = "l", xlab = "", ylab = "Global_reactive_power")
 
 # Write to PNG file
-dev.copy(png, file = "./Exploratory Data Analysis/Project 1/plot4.png", width = 480, height = 480)
+dev.copy(png, file = "plot4.png", width = 480, height = 480)
 dev.off()
